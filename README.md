f6cf5bb3725420ec49bf3bf22f8c11885218723f
Arman Ortega <ortegaaa@ph.ibm.com>
2016-12-18 19:26:46 +0800

    Update main.jsx, main.html

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 #### How to install everything:   
 $ npm install   
-Once installed successfully, you can run the app in two(2) different ways:
-$ npm run build   <em>// runs the Webpack in production mode</em>   
-$ npm run dev     // runs the Webpack in the watch mode   
 
-When running `npm run build` make sure that you physically delete the files inside build/ folder.
+Once installed successfully, you can run the app in two(2) different ways:    
+$ npm run build&nbsp;&nbsp;&nbsp;<em><-- runs the Webpack in production mode</em>   
+$ npm run dev&nbsp;&nbsp;&nbsp;&nbsp;<em><-- runs the Webpack in the watch mode</em>   
+
+Before running `npm run build` make sure that you physically delete first the files inside build/ folder.
 
 #### Runs the webpack in the watch mode which will automatically bundle the file whenever there is a change detected then open the main.html in your browser
 $ webpack -d --watch or npm run dev   
@@ -18,5 +19,3 @@ $ npm install babel-core --save
 $ npm install babel-loader babel-preset-es2015 babel-preset-react -S   
 $ npm install webpack --save-dev   
 $ npm install webpack-dev-server --save-dev   
-
-
--- main.jsx
+++ main.jsx
@@ -23,16 +23,17 @@ class ParentComponent extends React.Component {
 
 
 class ChildComponent extends React.Component {
-    
   render () {
     var self = this;
-    console.log('ChildComponent n:',this.props.number);
+    console.log('ChildComponent n:',self.props.number);
     return (
-      <div className="child" key={this.props.number}>
-        {"I am child " + this.props.number}
+      <div className="child" key={self.props.number}>
+        <div className="contentData">
+          <input type="text" defaultValue={"I am child " + self.props.number} />
+        </div>
         <div className="actionBtn">
             <a href="#"><img src="public/img/save-ico.png"></img></a>
-            <a href="#" onClick={self.props.deleteChild} ><img src="public/img/delete-ico.png"></img></a>
+            <a href="#" onClick={self.props.deleteChild.bind(this, self.props.number)} ><img src="public/img/delete-ico.png"></img></a>
         </div>
       </div>
     );
@@ -44,7 +45,8 @@ class AppComponent extends React.Component {
   constructor () {
     super();
     this.state = {
-      numChildren: 0
+      numChildren: 0,
+      childIdx: 0
     };
   }
 
@@ -52,17 +54,39 @@ class AppComponent extends React.Component {
     this.setState({
       numChildren: this.state.numChildren + 1
     });
+    this.setState({
+      childIdx: this.state.childIdx + 1
+    });
+  }
+
+  deleteChild(idx) {
+    var ctr = 0;
+    if (this.state.numChildren != 0) {
+      ctr = this.state.numChildren - 1;
+    }
+    console.log('deleteChild..idx:',idx);
+
+    this.setState({
+      numChildren: ctr
+    });
+    this.setState({
+      childIdx: idx
+    });
+    console.log('children :',children);
   }
 
   render() {
     const children = [];
+    var self = this;
+    // var index = children.indexOf(self.state.childIdx);
+    // var index = self.state.childIdx;
 
-    for (var i = 0; i < this.state.numChildren; i += 1) {
-      children.push(<ChildComponent number={i} key={i} />);
+    for (var i = 0; i < self.state.numChildren; i += 1) {
+      children.push(<ChildComponent number={i} key={i} deleteChild={self.deleteChild.bind(this)} />);
     };
 
     return (
-      <ParentComponent addChild={this.onAddChild.bind(this)}>
+      <ParentComponent addChild={self.onAddChild.bind(this)}>
         {children}
       </ParentComponent>
     );
